3d6b8bd7fc46107570a62971b757b12d9b48c195
Yash Sharma <yash@Yashs-MacBook-Air.local>
2022-11-16 10:22:46 -0500

    Merge optimiser into entanglement_measure

--- locc/example_ghz.py
+++ locc/example_ghz.py
@@ -1,5 +1,6 @@
 from qiskit.quantum_info import Statevector
 import numpy as np
+from entanglement_measures import EntanglementMeasures
 
 def GHZ(dims):
     psi = np.zeros((dims, dims, dims))
@@ -12,3 +13,6 @@ def GHZ(dims):
     #print("Norm of state =", norm(psi.flatten()))
 
 #now we compute the nursing numbers for this state
+
+em = EntanglementMeasures(2, GHZ(2), 2)
+em.get_le_upper_bound(GHZ(2), 0, 1)
