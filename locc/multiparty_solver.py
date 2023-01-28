207dae6fc7d3a2c747885e898eb0ce4c473ce2ab
Yash Sharma <yash@yashs-air.mynetworksettings.com>
2023-01-27 19:58:04 -0500

    Moved intervals creation to class

--- locc/multiparty_solver.py
+++ locc/multiparty_solver.py
@@ -7,10 +7,30 @@ class multiparty_solver:
     def __init__(self, N):
         self.N = N
         return
-    
-    def run_over_intervals(self):
-       return
 
+    def get_intervals(self):
+        intervals = []
+        for partition in ms.bipartitions():
+            #check if partition is valid
+            is_valid = True
+
+            for node in partition[0]:
+                if len(partition[0]) > 1:
+                    if (node+1 not in partition[0]) and (node-1 not in partition[0]):
+                        is_valid = False
+            
+            if is_valid:
+                for node in partition[1]:
+                    if len(partition[1]) > 1:
+                        if (node+1 not in partition[1]) and (node-1 not in partition[1]):
+                            is_valid = False
+
+
+            if is_valid:
+                intervals.append(partition)
+
+        print(intervals)
+        return intervals
 
     def bipartitions(self):
         for i in range(1, self.N):
@@ -32,9 +52,7 @@ class multiparty_solver:
         print(edge_ids)
         return edges, edge_ids
 
-    def compute_entropies(self, edges, edge_ids, intervals):
-        k_party_obj = k_party(4, 2, [(1, [2]), (1, [2]), (1, [2]), (1, [2])], random_statevector(16))
-
+    def compute_entropies_for_intervals(self, k_party_obj, edges, edge_ids, intervals):
         mat = np.zeros((len(intervals), len(intervals)))
 
         entropy_matrix = np.zeros((len(intervals), 1))
@@ -60,28 +78,7 @@ class multiparty_solver:
         print(p)
 
 ms = multiparty_solver(4)
-
-intervals = []
-for partition in ms.bipartitions():
-    #check if partition is valid
-    is_valid = True
-
-    for node in partition[0]:
-        if len(partition[0]) > 1:
-            if (node+1 not in partition[0]) and (node-1 not in partition[0]):
-                is_valid = False
-     
-    if is_valid:
-        for node in partition[1]:
-            if len(partition[1]) > 1:
-                if (node+1 not in partition[1]) and (node-1 not in partition[1]):
-                    is_valid = False
-
-
-    if is_valid:
-        intervals.append(partition)
-
-print(intervals)
-
+intervals = ms.get_intervals()
 edges, edge_ids = ms.create_circular_graph()
-ms.compute_entropies(edges, edge_ids, intervals)
+k_party_obj = k_party(4, 2, [(1, [2]), (1, [2]), (1, [2]), (1, [2])], random_statevector(16))
+ms.compute_entropies_for_intervals(k_party_obj, edges, edge_ids, intervals)
