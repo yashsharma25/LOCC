80467785ea5dbb3fe0f3376f13e0ae0c0b8daf3a
Yash Sharma <yash@yashs-air.mynetworksettings.com>
2023-01-30 10:35:29 -0500

    Return edge endpoints and weights in a map

--- locc/multiparty_solver.py
+++ locc/multiparty_solver.py
@@ -1,6 +1,7 @@
 import numpy as np
 from itertools import combinations
 from qiskit.quantum_info import random_statevector
+from qiskit.quantum_info import Statevector
 import networkx as nx
 from k_party import k_party
 
@@ -11,26 +12,62 @@ class multiparty_solver:
 
     def get_intervals(self):
         intervals = []
+        
+        #except the cut vertices all other nodes should have both their neighbours in the same partition
         for partition in ms.bipartitions():
-            #check if partition is valid
+            #check if partition is valid i.e if the interval is contigous
             is_valid = True
 
-            # for node in partition[0]:
-            #     if len(partition[0]) > 1:
-            #         if (node+1 not in partition[0]) and (node-1 not in partition[0]):
-            #             is_valid = False
-            
-            # if is_valid:
-            #     for node in partition[1]:
-            #         if len(partition[1]) > 1:
-            #             if (node+1 not in partition[1]) and (node-1 not in partition[1]):
-            #                 is_valid = False
+            cut_vertices = 0
+            if len(partition[0]) == 1 or len(partition[1]) == 1:
+                is_valid = True
 
+            else:
+                for node in partition[0]:
+                    neighbor_1 = node + 1
+                    neighbor_2 = node - 1
 
-            
-            intervals.append(partition)
+                    if node == 0:
+                        neighbor_2 = self.N - 1
+
+                    elif node == self.N-1:
+                        neighbor_1 = 0
+
+                    if (neighbor_1 not in partition[0]) or (neighbor_2 not in partition[0]):
+                        #its okay if this is a cut
+                        cut_vertices += 1
+
+                        if cut_vertices > 2:
+                            is_valid = False
+                            break
+
+                if is_valid:
+                    cut_vertices = 0
+
+                    for node in partition[1]:
+                        neighbor_1 = node + 1
+                        neighbor_2 = node - 1
+
+                        if node == 0:
+                            neighbor_2 = self.N - 1
+
+                        elif node == self.N-1:
+                            neighbor_1 = 0
+
+                        if (neighbor_1 not in partition[1]) or (neighbor_2 not in partition[1]):
+                            cut_vertices += 1
+
+                            if cut_vertices > 2:
+                                is_valid = False
+                                break
+                
+            if is_valid:
+                intervals.append(partition)
 
         #there are twice as many intervals generated because in the second half, just the order is flipped
+        print("len(intervals) = ", len(intervals))
+        # for i in intervals:
+        #     print(i)
         list_half = round(len(intervals)/2)
         intervals = intervals[:list_half]
         return intervals
@@ -42,10 +79,11 @@ class multiparty_solver:
                 yield (set(comb), set(range(self.N)) - set(comb))
 
 
-    def create_circular_graph(self):
+    def create_complete_graph(self):
         G = nx.complete_graph(self.N)
         edge_ids = {}
         id = 0
+        print("Num of edges = ", len(G.edges()))
         for e in G.edges():
             edge_ids[(e[0], e[1])] = id
             edge_ids[(e[1], e[0])] = id
@@ -54,34 +92,49 @@ class multiparty_solver:
         #print(edge_ids)
         return G.edges(), edge_ids
 
-    def compute_entropies_for_intervals(self, k_party_obj, edges, edge_ids, intervals):
-        mat = np.zeros((len(intervals), len(intervals)))
+    '''
+    Returns:
+    A dictionary where the key is the two vertices of an edge, and the value is the weight
+    '''
+    def compute_entropies_for_intervals(self, k_party_obj, edges, edge_weights_maps, intervals):
+
+        mat = np.zeros((len(intervals), len(edges)))
 
         entropy_matrix = np.zeros((len(intervals), 1))
         for index, intvl in enumerate(intervals):
-            print("Interval = ", intvl[0], intvl[1])
+            #print("Interval = ", intvl[0], intvl[1])
             for e in edges:
                 if ((e[0] in intvl[0]) and (e[1] in intvl[1])) or ((e[1] in intvl[0]) and (e[0] in intvl[1])):
                     #now we know this is a crossing edge
                     #set this edgeId = 1 in the mat
-                    mat[index][edge_ids[(e[0], e[1])]] = 1
+                    mat[index][edge_weights_maps[(e[0], e[1])]] = 1
 
             entropy_matrix[index] = k_party_obj.bipartite_entropy(list(intvl[0]), list(intvl[1]))
 
 
-        print(mat)
-        print("________________________")
-        print(entropy_matrix)
+        # print(mat)
+        # print("________________________")
+        # print(entropy_matrix)
       
         print("Shape of mat = ", mat.shape)
         print("Shape of entropy matrix = ", entropy_matrix.shape)
 
-        p, res, rank, s = np.linalg.lstsq(mat, entropy_matrix)
-        print(p)
+        weights, res, rank, s = np.linalg.lstsq(mat, entropy_matrix)
+        #print(p)
+        #return the edge weights and edge nodes
+        for index, e in enumerate(edges):
+            edge_weights_maps[(e[0], e[1])] = weights[index]
+            edge_weights_maps[(e[1], e[0])] = weights[index]
+
+
+        # for e in edge_weights_maps:
+        #     print(e, edge_weights_maps[e])
+        return weights
         
 
 ms = multiparty_solver(10)
 intervals = ms.get_intervals()
-edges, edge_ids = ms.create_circular_graph()
+edges, edge_ids = ms.create_complete_graph()
 k_party_obj = k_party(10, 2, [(1, [2]), (1, [2]), (1, [2]), (1, [2]), (1, [2]), (1, [2]), (1, [2]), (1, [2]),  (1, [2]), (1, [2])], random_statevector(2**10))
+
 ms.compute_entropies_for_intervals(k_party_obj, edges, edge_ids, intervals)
